1516c851d0b6157468107b69ad8d3d42bd541e9a
Виктор Илић <121385046+Anonymous6598@users.noreply.github.com>
2024-12-06 23:39:29 +0100

    Add files via upload

--- My_Maps/My_Maps.py
+++ My_Maps/My_Maps.py
@@ -1,4 +1,4 @@
-import customtkinter, tkintermapview, typing, geocoder, My_Maps_AI, asyncio, tkinter, CTkMenuBar, locale, My_Maps_Interface
+import customtkinter, tkintermapview, typing, geocoder, CTkMenuBar, locale, My_Maps_Interface, os
 
 class Program(customtkinter.CTk, My_Maps_Interface.My_Maps_Interface):
     
@@ -30,16 +30,15 @@ class Program(customtkinter.CTk, My_Maps_Interface.My_Maps_Interface):
 
         if locale.getdefaultlocale()[0] == f"sr_RS":
             self.main_screen_menu_dropdownmenu.add_option(option=f"🔎 (претрага)", command=self.__search__)
-            self.main_screen_menu_dropdownmenu.add_option(option=f"ВИ", command=lambda: AI_Window())
+            self.main_screen_menu_dropdownmenu.add_option(option=f"AI", command=lambda: os.startfile(f"My_Maps_AI_window.py", show_cmd=False))
         
         elif locale.getdefaultlocale()[0] == f"ru_RU":
             self.main_screen_menu_dropdownmenu.add_option(option=f"🔎 (поиск)", command=self.__search__)
-            self.main_screen_menu_dropdownmenu.add_option(option=f"ИИ (Нейро сеть)", command=lambda: AI_Window())
+            self.main_screen_menu_dropdownmenu.add_option(option=f"ИИ (Нейро сеть)", command=lambda: os.startfile(f"My_Maps_AI_window.py", show_cmd=False))
         
         else:
             self.main_screen_menu_dropdownmenu.add_option(option=f"🔎 (search)", command=self.__search__)
-            self.main_screen_menu_dropdownmenu.add_option(option=f"AI", command=lambda: AI_Window())
-
+            self.main_screen_menu_dropdownmenu.add_option(option=f"AI", command=lambda: os.startfile(f"My_Maps_AI_window.py", show_cmd=False))
 
     @typing.override
     def __search__(self: typing.Self) -> None:
@@ -61,40 +60,5 @@ class Program(customtkinter.CTk, My_Maps_Interface.My_Maps_Interface):
             
             self.main_screen_map.set_address(self.main_screen_search_dialog.get_input())
         
-class AI_Window(customtkinter.CTkToplevel):
-
-	TITLE: typing.Final[str] = f"My Maps AI assistant"
-	HEIGHT: typing.Final[int] = 375
-	WIDTH: typing.Final[int] = 655
-	ICON: typing.Final[str] = f"my maps icon.ico"
-
-	def __init__(self: typing.Self, *args, **kwargs) -> None:
-		customtkinter.CTkToplevel.__init__(self, *args, **kwargs)
-
-		self.title(self.TITLE)
-		self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
-		self.resizable(False, False)
-		self.after(250, lambda: self.iconbitmap(self.ICON))
-
-		self.ai_window_textbox: customtkinter.CTkTextbox = customtkinter.CTkTextbox(master=self, height=265, width=524, corner_radius=0, fg_color=f"transparent", text_color=(f"black", f"white"))
-		self.ai_window_textbox.place(x=0, y=0)
-
-		self.ai_window_textbox.configure(state=f"disabled")
-
-		self.ai_window_entry: customtkinter.CTkEntry = customtkinter.CTkEntry(master=self, height=30, width=524, border_width=0, fg_color=f"transparent", placeholder_text=f"...")
-		self.ai_window_entry.place(x=0, y=269)
-
-		self.ai_window_entry.bind(f"<Return>", self.__response__)
-
-	def __response__(self: typing.Self, configure: str | None = None) -> None:
-		self.ai_window_entry_data: str = self.ai_window_entry.get()
-
-		self.ai_window_textbox.configure(state=f"normal")
-		self.query: str = asyncio.run(My_Maps_AI.My_Maps_LM().__response__(self.ai_window_entry_data))
-
-		self.ai_window_textbox.insert(tkinter.END, f"{self.query}\n", f"-1.0")
-		self.ai_window_textbox.configure(state=f"disabled")
-		self.ai_window_entry.delete(f"-1", tkinter.END)
-
 if __name__ == f"__main__":
     program: Program = Program().mainloop()
