75d48b53ad72026a0ba6da8b5e9ab89c0ecf128f
Виктор Илић <vikiilitch@outlook.com>
2025-04-22 00:13:34 +0200

    Delete My_Maps directory

--- My_Maps/Linux/My_Maps_AI_window_interface.py
+++
@@ -1,9 +0,0 @@
-import abc, typing
-
-class My_Maps_AI_window_interface(abc.ABC):
-
-    def __response__(self: typing.Self, configure: str | None = None) -> None:
-        pass
-
-    def __audio_input__(self: typing.Self) -> None:
-        pass
